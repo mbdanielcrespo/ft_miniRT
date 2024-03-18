dc8f30d4ab6cee229d04930a6dbdf0e2411ad5ae
danielmola <mbdanielcrespo@gmail.com>
2024-03-18 23:21:07 +0000

    commiting

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danalmei <danalmei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/17 20:38:13 by danalmei          #+#    #+#             */
-/*   Updated: 2024/03/17 22:05:37 by danalmei         ###   ########.fr       */
+/*   Created: 2024/03/17 21:33:09 by danalmei          #+#    #+#             */
+/*   Updated: 2024/03/17 21:33:10 by danalmei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAIN_H
-# define MAIN_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <libft.h>
-
-
-#endif //MAIN_H
