3428c477ed57761d9a713037b91bd0be37af0e04
Filipe Silva <filipeedensilva@gmail.com>
2024-05-07 15:15:37 +0100

    Added check for file type

--- src/parsing/data.c
+++ src/parsing/data.c
@@ -6,7 +6,7 @@
 /*   By: danalmei <danalmei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 16:21:56 by danalmei          #+#    #+#             */
-/*   Updated: 2024/04/29 18:36:13 by danalmei         ###   ########.fr       */
+/*   Updated: 2024/05/07 15:13:36 by feden-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,18 @@ void	init_data(char **av)
 void	open_and_read_file(char **av)
 {
 	int	fd;
+	int	i;
+	int	j;
+	char	*rt;
 
+	rt = ".rt";
+	i = ft_strlen(av[1]) - 3;
+	j = 0;
+	while (av[1][i])
+	{
+		if (av[1][i++] != rt[j++])
+			ft_error("Invalid file type!");
+	}
 	fd = open(av[1], O_RDONLY);		// TODO: DONT FORGET TO CLOSE FD ON DATA DESTROY
 	if (fd < 0)
 		ft_error("Error opening the file!");	// TODO: CHECK IF FILE HAS .rt EXTENSION 
